dace7e6d40883854e41314b9d6b434a7dd0ec64f
Haifeng Chen <chenhaifeng88888@gmail.com>
2024-12-05 20:05:08 -0800

    Reapply "[KCP] delay processing for original topic"
    Summary:
    This reverts commit f70603786b7c482b724d7e77d4e041b638f0760f.
    
    The previous "The topic partition is already in delayedRecords" issue happens when the unprocessedRecords in delayedRecord are resumed processing, but the topic partitions have not yet resumed in KafkaConsumer and removed from delayRecord. At that case, when the first unprocessed records is processed, the second to the last records may need delay and will throw the above error.
    
    Fix it in KafkaDelayProcessManager.java line 74.

--- uforwarder-core/src/test/java/com/uber/data/kafka/datatransfer/worker/fetchers/kafka/KafkaDelayProcessManagerTest.java
+++ uforwarder-core/src/test/java/com/uber/data/kafka/datatransfer/worker/fetchers/kafka/KafkaDelayProcessManagerTest.java
@@ -1,6 +1,7 @@
 package com.uber.data.kafka.datatransfer.worker.fetchers.kafka;
 
 import com.uber.fievel.testing.base.FievelTestBase;
+import com.uber.m3.tally.Counter;
 import com.uber.m3.tally.Gauge;
 import com.uber.m3.tally.Scope;
 import java.util.ArrayList;
@@ -29,8 +30,10 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
   public void setUp() {
     scope = Mockito.mock(Scope.class);
     Gauge gauge = Mockito.mock(Gauge.class);
+    Counter counter = Mockito.mock(Counter.class);
     Mockito.when(scope.tagged(ArgumentMatchers.anyMap())).thenReturn(scope);
     Mockito.when(scope.gauge(ArgumentMatchers.anyString())).thenReturn(gauge);
+    Mockito.when(scope.counter(ArgumentMatchers.anyString())).thenReturn(counter);
     mockConsumer = Mockito.mock(KafkaConsumer.class);
   }
 
@@ -76,7 +79,7 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
 
-    delayProcessManager.pause(topicPartition, unprocessedRecords);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords);
     Assert.assertEquals(2, delayProcessManager.getRecords(topicPartition).size());
 
     TopicPartition topicPartition2 = new TopicPartition("test-topic", 2);
@@ -92,7 +95,7 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
 
-    delayProcessManager.pause(topicPartition, unprocessedRecords);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords);
 
     List<TopicPartition> pausedPartitions = delayProcessManager.getAll();
     Assert.assertEquals(1, pausedPartitions.size());
@@ -100,6 +103,37 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     delayProcessManager
         .getRecords(topicPartition)
         .forEach(record -> Assert.assertTrue(unprocessedRecords.contains(record)));
+  }
+
+  @Test
+  public void testPauseWithRepeatTopiPartitions() {
+    int processingDelayMs = 10000; // 10 seconds
+    delayProcessManager =
+        new KafkaDelayProcessManager<>(scope, consumerGroup, processingDelayMs, mockConsumer);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, new ArrayList<>());
+    List<ConsumerRecord<K, V>> unprocessedRecords1 = new ArrayList<>();
+    unprocessedRecords1.add(Mockito.mock(ConsumerRecord.class));
+    unprocessedRecords1.add(Mockito.mock(ConsumerRecord.class));
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords1);
+
+    List<ConsumerRecord<K, V>> unprocessedRecords2 = new ArrayList<>();
+    unprocessedRecords2.add(Mockito.mock(ConsumerRecord.class));
+    unprocessedRecords2.add(Mockito.mock(ConsumerRecord.class));
+    try {
+      delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords2);
+      Assert.fail("The topic partition test-topic-1 is already in delayedRecords");
+    } catch (IllegalStateException e) {
+      // expected
+    }
+  }
+
+  @Test
+  public void testPauseWithEmtpyUnprocessedRecords() {
+    int processingDelayMs = 10000; // 10 seconds
+    delayProcessManager =
+        new KafkaDelayProcessManager<>(scope, consumerGroup, processingDelayMs, mockConsumer);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, new ArrayList<>());
+    Mockito.verify(mockConsumer, Mockito.times(0)).pause(Collections.singleton(topicPartition));
   }
 
   @SuppressWarnings("ForbidTimedWaitInTests") // Initial enrollment
@@ -113,7 +147,7 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     ConsumerRecord<K, V> consumerRecord1 = Mockito.mock(ConsumerRecord.class);
     Mockito.when(consumerRecord1.timestamp()).thenReturn(System.currentTimeMillis());
     unprocessedRecords1.add(consumerRecord1);
-    delayProcessManager.pause(topicPartition, unprocessedRecords1);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords1);
 
     List<ConsumerRecord<K, V>> unprocessedRecords2 = new ArrayList<>();
     TopicPartition topicPartition2 = new TopicPartition("test-topic", 2);
@@ -121,17 +155,18 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     Mockito.when(consumerRecord2.timestamp())
         .thenReturn(System.currentTimeMillis() + 25000); // 25 seconds
     unprocessedRecords2.add(consumerRecord2);
-    delayProcessManager.pause(topicPartition2, unprocessedRecords2);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition2, unprocessedRecords2);
 
     Thread.sleep(10000); // 10 seconds
 
-    Map<TopicPartition, List<ConsumerRecord<K, V>>> resumedRecords1 = delayProcessManager.resume();
+    Map<TopicPartition, List<ConsumerRecord<K, V>>> resumedRecords1 =
+        delayProcessManager.resumePausedPartitionsAndRecords();
     Assert.assertEquals(1, resumedRecords1.size());
     Assert.assertEquals(topicPartition, resumedRecords1.keySet().iterator().next());
     Assert.assertEquals(unprocessedRecords1, resumedRecords1.values().iterator().next());
 
     // Processed topic partitions should be deleted in the next round
-    delayProcessManager.resume();
+    delayProcessManager.resumePausedPartitionsAndRecords();
     Assert.assertEquals(1, delayProcessManager.getAll().size());
     Assert.assertEquals(topicPartition2, delayProcessManager.getAll().get(0));
   }
@@ -144,13 +179,13 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     List<ConsumerRecord<K, V>> unprocessedRecords = new ArrayList<>();
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
-    delayProcessManager.pause(topicPartition, unprocessedRecords);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords);
 
     TopicPartition topicPartition2 = new TopicPartition("test-topic", 2);
     List<ConsumerRecord<K, V>> unprocessedRecords2 = new ArrayList<>();
     unprocessedRecords2.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords2.add(Mockito.mock(ConsumerRecord.class));
-    delayProcessManager.pause(topicPartition2, unprocessedRecords2);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition2, unprocessedRecords2);
 
     delayProcessManager.delete(Collections.singletonList(topicPartition));
     List<TopicPartition> topicPartitions = delayProcessManager.getAll();
@@ -166,7 +201,7 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     List<ConsumerRecord<K, V>> unprocessedRecords = new ArrayList<>();
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
-    delayProcessManager.pause(topicPartition, unprocessedRecords);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords);
     delayProcessManager.close();
     Assert.assertTrue(delayProcessManager.getAll().isEmpty());
   }
@@ -179,7 +214,7 @@ public class KafkaDelayProcessManagerTest<K, V> extends FievelTestBase {
     List<ConsumerRecord<K, V>> unprocessedRecords = new ArrayList<>();
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
     unprocessedRecords.add(Mockito.mock(ConsumerRecord.class));
-    delayProcessManager.pause(topicPartition, unprocessedRecords);
+    delayProcessManager.pausedPartitionsAndRecords(topicPartition, unprocessedRecords);
     delayProcessManager.close();
     Assert.assertTrue(delayProcessManager.getAll().isEmpty());
   }
