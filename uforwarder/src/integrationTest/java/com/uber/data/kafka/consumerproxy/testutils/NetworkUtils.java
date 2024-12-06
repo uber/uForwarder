ba63ffadf2599cb8c92ff7e7c0955106ec4fd5b4
Haifeng Chen <chenhaifeng88888@gmail.com>
2024-12-05 20:05:28 -0800

    uforwarder integration test random port from dynamic port range
    Summary: as uforwarder integration test already stabilized, we can revert some previous change that doesn't need anymore

--- uforwarder/src/integrationTest/java/com/uber/data/kafka/consumerproxy/testutils/NetworkUtils.java
+++ uforwarder/src/integrationTest/java/com/uber/data/kafka/consumerproxy/testutils/NetworkUtils.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,10 +16,6 @@ import org.testcontainers.containers.Network;
 /** Utilities for network related stuffs during unit tests */
 public class NetworkUtils {
   private static final Logger logger = LoggerFactory.getLogger(NetworkUtils.class);
-  private static final int MAX_RANDOM_PORT_ATTEMPT = 3; // number of attempts to find random port
-  private static final int MIN_RANDOM_PORT = 30000; // start of random port
-  private static final int MAX_RANDOM_PORT =
-      32768; // end of random port also start of ephemeral port in linux
   /**
    * Validates the port is in use
    *
@@ -33,18 +28,17 @@ public class NetworkUtils {
     }
   }
 
+  /**
+   * Gets random available port.
+   *
+   * @return the random available port
+   */
   public static synchronized int getRandomAvailablePort() {
-    for (int i = 0; i < MAX_RANDOM_PORT_ATTEMPT; ++i) {
-      int port = RandomUtils.nextInt(MIN_RANDOM_PORT, MAX_RANDOM_PORT);
-      try (ServerSocket socket = new ServerSocket(port)) {
-        return socket.getLocalPort();
-      } catch (IOException e) {
-        logger.warn(String.format("Failed to open socket on port %d, attempt = %d", port, i), e);
-        // retry if port is not available
-      }
+    try (ServerSocket socket = new ServerSocket(0); ) {
+      return socket.getLocalPort();
+    } catch (IOException e) {
+      throw new IllegalStateException(e);
     }
-
-    throw new IllegalStateException("Failed to find available port");
   }
 
   /**
