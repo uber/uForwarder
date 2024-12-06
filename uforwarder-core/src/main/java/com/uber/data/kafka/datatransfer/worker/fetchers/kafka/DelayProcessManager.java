133ed3b6210eabe2f215c4ec2dbe07f3cb657e4a
Haifeng Chen <chenhaifeng88888@gmail.com>
2024-12-05 20:04:08 -0800

    Refactor DelayProcessManager NOOP.

--- uforwarder-core/src/main/java/com/uber/data/kafka/datatransfer/worker/fetchers/kafka/DelayProcessManager.java
+++ uforwarder-core/src/main/java/com/uber/data/kafka/datatransfer/worker/fetchers/kafka/DelayProcessManager.java
@@ -2,6 +2,7 @@ package com.uber.data.kafka.datatransfer.worker.fetchers.kafka;
 
 import java.io.Closeable;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -12,6 +13,47 @@ import org.apache.kafka.common.TopicPartition;
  * records.
  */
 public interface DelayProcessManager<K, V> extends Closeable {
+
+  /* NOOP implements a default DelayProcessManager which does nothing */
+  DelayProcessManager NOOP =
+      new DelayProcessManager() {
+
+        // Default implementation for checking whether a topic partition should be delayed.
+        @Override
+        public boolean shouldDelayProcess(ConsumerRecord record) {
+          return false;
+        }
+
+        // Default implementation to pause a topic partition. This operation is not supported.
+        // It should never reach here as shouldDelayProcess function always returns false.
+        @Override
+        public void pause(TopicPartition tp, List unprocessedRecords) {
+          throw new UnsupportedOperationException();
+        }
+
+        // Default implementation for resuming paused topic partitions.
+        // It returns an empty list as shouldDelayProcess function always returns false.
+        @Override
+        public Map<TopicPartition, List<ConsumerRecord>> resume() {
+          return Collections.emptyMap();
+        }
+
+        // Default implementation for deleting paused topic partitions.
+        @Override
+        public void delete(Collection tps) {
+          throw new UnsupportedOperationException("This operation is not supported");
+        }
+
+        // Default implementation for getting paused topic partitions.
+        @Override
+        public List<TopicPartition> getAll() {
+          return Collections.emptyList();
+        }
+
+        // Default implementation for deleting paused topic partitions.
+        @Override
+        public void close() {}
+      };
 
   /**
    * Determines whether to delay processing a record based on the delay time and record timestamp.
