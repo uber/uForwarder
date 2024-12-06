c7d39a0776da7a59711414f6acfd130d143b0169
Haifeng Chen <chenhaifeng88888@gmail.com>
2024-12-05 20:04:59 -0800

    Revert "[KCP] delay processing for original topic"
    Summary:
    This reverts commit 360879d87d5011dccf9597538599fd876b23e419.
    
    We observed errors in original topic with delay processing as retry queue reported by a user.

--- uforwarder/src/test/java/com/uber/data/kafka/consumerproxy/worker/fetcher/OriginalTopicKafkaFetcherTest.java
+++ uforwarder/src/test/java/com/uber/data/kafka/consumerproxy/worker/fetcher/OriginalTopicKafkaFetcherTest.java
@@ -11,7 +11,6 @@ import com.uber.data.kafka.datatransfer.common.DynamicConfiguration;
 import com.uber.data.kafka.datatransfer.worker.common.PipelineStateManager;
 import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.AbstractKafkaFetcherThread;
 import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.CheckpointManager;
-import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.DelayProcessManager;
 import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.KafkaFetcherConfiguration;
 import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.SeekStartOffsetOption;
 import com.uber.data.kafka.datatransfer.worker.fetchers.kafka.ThroughputTracker;
@@ -91,7 +90,6 @@ public class OriginalTopicKafkaFetcherTest extends FievelTestBase {
             kafkaFetcherConfiguration,
             checkpointManager,
             throughputTracker,
-            DelayProcessManager.NOOP,
             mockConsumer,
             infra);
     fetcherThread.setPipelineStateManager(pipelineStateManager);
@@ -105,7 +103,6 @@ public class OriginalTopicKafkaFetcherTest extends FievelTestBase {
         GROUP,
         AutoOffsetResetPolicy.AUTO_OFFSET_RESET_POLICY_EARLIEST,
         IsolationLevel.ISOLATION_LEVEL_UNSET,
-        -1,
         kafkaFetcherConfiguration,
         false,
         infra);
