9c71375b7307faac03882da25e4814876f7b0dd6
Zhifeng Chen <chenz@uber.com>
2025-10-04 13:27:08 -0700

    [KCP] health check framework PipelineHealthState
    Summary:
    [KCP] health check framework PipelineHealthState
    
    [[ https://docs.google.com/document/d/1xOz4nEQnmV0gVgrQ6O1yekh8C_EQD73i7FjFAP50ChY/edit?tab=t.0#heading=h.9h4ggmvsh0g5 | ERD ]]
    
    Test Plan: unit test
    
    Reviewers: #ldap_owner_kafka_monorepo_blocking, #ldap_owner_kafka_monorepo_nonblocking, hf
    
    Reviewed By: #ldap_owner_kafka_monorepo_blocking, #ldap_owner_kafka_monorepo_nonblocking, hf
    
    Subscribers: qichao, hf
    
    JIRA Issues: KAFEP-7739
    
    Differential Revision: https://code.uberinternal.com/D17080703

---
+++ uforwarder-core/src/test/java/com/uber/data/kafka/datatransfer/worker/pipelines/PipelineHealthStateTest.java
@@ -0,0 +1,45 @@
+package com.uber.data.kafka.datatransfer.worker.pipelines;
+
+import com.uber.data.kafka.datatransfer.common.TestUtils;
+import com.uber.fievel.testing.base.FievelTestBase;
+import java.time.Duration;
+import java.util.Set;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PipelineHealthStateTest extends FievelTestBase {
+  private TestUtils.TestTicker ticker;
+
+  private PipelineHealthState pipelineHealthState;
+
+  private int windowSizeSeconds = 10;
+  private int windowCount = 3;
+
+  @Before
+  public void setUp() {
+    ticker = new TestUtils.TestTicker();
+    pipelineHealthState =
+        new PipelineHealthState(ticker, Duration.ofSeconds(windowSizeSeconds), windowCount);
+  }
+
+  @Test
+  public void testRecordIssue() {
+    // TODO implement test
+    // Record the issue
+    pipelineHealthState.recordIssue(PipelineHealthIssue.INFLIGHT_MESSAGE_LIMITED);
+    Assert.assertEquals(0L, pipelineHealthState.getStateValue());
+  }
+
+  @Test
+  public void testWindowRecord() {
+    PipelineHealthState.MutableHealthStateWindow window =
+        pipelineHealthState.new MutableHealthStateWindow();
+    window.recordIssue(PipelineHealthIssue.INFLIGHT_MESSAGE_LIMITED);
+    window.recordIssue(PipelineHealthIssue.PERMISSION_DENIED);
+    int value = window.getValue();
+    Assert.assertEquals(
+        Set.of(PipelineHealthIssue.INFLIGHT_MESSAGE_LIMITED, PipelineHealthIssue.PERMISSION_DENIED),
+        PipelineHealthIssue.decode(value));
+  }
+}
